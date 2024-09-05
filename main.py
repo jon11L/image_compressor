ee1f9b7d568e051b2345d6d4ffcbad7335372a3f
jonathan <jonathan.lemari@gmail.com>
2024-09-05 05:53:13 +0200

    fixed some bug with exception handling

--- main.py
+++ main.py
@@ -1,63 +1,57 @@
 from PIL import Image
 import os
 
+import time
+
 def compress_images(input_dir, output_dir, quality=int):
     '''takes images and compress them into a new diretory, with a parameter to adjust the compression size.'''
-    # Create the output directory if it doesn't exist.
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
 
-    # check if input dictionary is existing before proceeding to compress images
+    # Check if input dictionary is existing before proceeding.
     while True:
         try:
-            # if input directory doesn't exist/not found, then error is raise and user prompted to enter it again.
+            # If input directory doesn't exist/not found, then error is raise and user prompted to enter it again.
             if not os.path.exists(input_dir):
-                raise FileNotFoundError(f"directory '{input_directory}' could not be found or does not exist.")
-            # directory exist, process the image files
-            for filename in os.listdir(input_dir):
-                if filename.lower().endswith(".jpg") or filename.lower().endswith(".jpeg"):
-                    filepath = os.path.join(input_dir, filename)
-                    image = Image.open(filepath)
-
-                    # Compress and save the image
-                    output_path = os.path.join(output_dir, filename)
-                    image.save(output_path, "JPEG", quality=quality)
-
-                    print(f"Compressed and saved to: {output_path}")
-            # exit the loop after processing
-            break
-
+                raise FileNotFoundError(f"directory '{input_dir}' could not be found or does not exist.")
+            break           
         except FileNotFoundError as e:
             print(e)
             input_dir = input("Please enter a valid input directory path: ")
 
+    # Create the output directory if it doesn't exist.
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Directory exist, process the image files
+    start_time = time.time()
+    for filename in os.listdir(input_dir):
+
+        if filename.lower().endswith(".jpg") or filename.lower().endswith(".jpeg"):
+            filepath = os.path.join(input_dir, filename)
+            image = Image.open(filepath)
+
+            # Compress and save the image
+            output_path = os.path.join(output_dir, filename)
+            try:
+                image.save(output_path, "JPEG", quality=quality)
+                end_time = time.time()
+                timing = end_time - start_time 
+                print(f"")
+                print(f"Compressed and saved to: {output_path}. In {round(timing, 4)}s") # In {timing} time
+            except Exception as save_error:
+                print(f"Failed to save {output_path}: {save_error}")
+                # os.
+            
+            
 
 
+# D:\programming\personal\projects
 
 
-
-
-
-
-
-
-
-        # Loop through all files in the input directory.
-        # for filename in os.listdir(input_dir):
-        #     if filename.lower().endswith(".jpg") or filename.lower().endswith(".jpeg"):
-        #         filepath = os.path.join(input_dir, filename)
-        #         image = Image.open(filepath)
-
-        #         # Compress and save the image
-        #         output_path = os.path.join(output_dir, filename)
-        #         image.save(output_path, "JPEG", quality=quality)
-
-        #         print(f"Compressed and saved: {output_path}")
 
 
 if __name__ == "__main__":
     input_directory = "path_to_your_input_directory"
-    output_directory = "image_compressed"
+    output_directory = "image_compressed" # If kept like this, that directory will be created where user currently is.
     quality = 30  # Adjust quality as needed, lower means more compression.
 
     compress_images(input_directory, output_directory, quality)
